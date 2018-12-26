4ec886b128efc7d1da1fc47dc13c694efb4100f9
Pablo De Jesus Moreno <pablo.24726@gmail.com>
2018-12-26 14:13:58 -0600

    Creación del timeline con sus respectivos artículos.
        Se creo la línea de tiempo por día, se le adjunto un artículo y se
          aplico el responsive web design

--- resources/views/layout/blog/layout.blade.php
+++ resources/views/layout/blog/layout.blade.php
@@ -21,65 +21,12 @@
     </header>
     <main class="uk-panel uk-padding" uk-height-viewport="expand: true" >
         <div class="uk-container">
-            <h1 class="uk-text-center">Blog</h1>
-            <section id="calendar" class="calendar uk-margin-auto">
-                <button id="calendar__button-preview" class="uk-button uk-button-secondary uk-button-small uk-border-rounded calendar__button">
-                    <span class="calendar__button--icon" uk-icon="icon: chevron-left; ratio: 1.5"></span>
-                </button>
-                <div id="dates" class="dates uk-margin-small-left uk-margin-small-right">
-                    <div id="dates-overflow--hidden" class="dates-overflow--hidden uk-overflow-auto">
-                        <table class="uk-table">
-                            <tbody>
-                                <tr>
-                                    <td id="dates__item--first" class="dates__item dates__item--first uk-padding-remove"></td>
-                                    <td class="dates__item dates__item--selected uk-padding-remove" data-numitem="1">
-                                        <a class="date__link dates__link--selected uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Abr, 2018</a>
-                                    </td>
-                                    <td class="dates__item uk-padding-remove" data-numitem="2">
-                                        <a class="date__link uk-margin-small-left uk-margin-small-right uk-text-center" href="#">May, 2018</a>
-                                    </td>
-                                    <td class="dates__item uk-padding-remove" data-numitem="3">
-                                        <a class="date__link uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Jun, 2018</a>
-                                    </td>
-                                    <td class="dates__item uk-padding-remove" data-numitem="4">
-                                        <a class="date__link uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Jul, 2018</a>
-                                    </td>
-                                    <td class="dates__item uk-padding-remove" data-numitem="5">
-                                        <a class="date__link uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Ago, 2018</a>
-                                    </td>
-                                    <td class="dates__item uk-padding-remove" data-numitem="6">
-                                        <a class="date__link uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Sep, 2018</a>
-                                    </td>
-                                    <td class="dates__item  uk-padding-remove" data-numitem="7">
-                                        <a class="date__link  uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Oct, 2018</a>
-                                    </td>
-                                    <td class="dates__item  uk-padding-remove" data-numitem="8">
-                                        <a class="date__link  uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Nov, 2018</a>
-                                    </td>
-                                    <td class="dates__item  uk-padding-remove" data-numitem="9">
-                                        <a class="date__link  uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Dec, 2018</a>
-                                    </td>
-                                    <td class="dates__item dates__item--end uk-padding-remove"></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <button id="calendar__button-next" class="uk-button uk-button-secondary uk-button-small uk-border-rounded calendar__button">
-                    <span uk-icon="icon: chevron-right; ratio: 1.5"></span>
-                </button>
-            </section>
-            <div class="uk-grid" style="background: red;">
-                <section class="uk-width-2-5 uk-text-right">articles</section>
-                <section class="days uk-width-1-5 uk-text-center">days</section>
-                <section class="uk-width-2-5">articles</section>
-            </div>
+            @yield('content')
         </div>
     </main>
     <footer class="uk-panel uk-padding uk-padding-small footer--background uk-light">
         <div class="uk-container"> <p class="uk-text-center"> Copyright </p> </div>
     </footer>
-
 
     <script src="{{ mix('/js/app.js') }}"></script>
     @yield('scripts')
--- resources/views/template/blog/index.blade.php
+++ resources/views/template/blog/index.blade.php
@@ -1,3 +1,132 @@
 @extends('layout.blog.layout')
 
 @section('title', 'Blog')
+
+@section('styles')
+	{{-- Ingrese los archivos CSS --}}
+@endsection
+
+@section('scripts')
+	{{-- Ingrese los archivos JavaScript --}}
+@endsection
+
+@section('content')
+	<h1 class="uk-text-center">Blog</h1>
+	<section id="calendar" class="calendar uk-margin-auto">
+		<button id="calendar__button-preview" class="uk-button uk-button-secondary uk-button-small uk-border-rounded calendar__button">
+			<span class="calendar__button--icon" uk-icon="icon: chevron-left; ratio: 1.5"></span>
+		</button>
+		<div id="dates" class="dates uk-margin-small-left uk-margin-small-right">
+			<div id="dates-overflow--hidden" class="dates-overflow--hidden uk-overflow-auto">
+				<table class="uk-table">
+					<tbody>
+						<tr>
+							<td id="dates__item--first" class="dates__item dates__item--first uk-padding-remove"></td>
+							<td class="dates__item dates__item--selected uk-padding-remove" data-numitem="1">
+								<a class="date__link dates__link--selected uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Abr, 2018</a>
+							</td>
+							<td class="dates__item uk-padding-remove" data-numitem="2">
+								<a class="date__link uk-margin-small-left uk-margin-small-right uk-text-center" href="#">May, 2018</a>
+							</td>
+							<td class="dates__item uk-padding-remove" data-numitem="3">
+								<a class="date__link uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Jun, 2018</a>
+							</td>
+							<td class="dates__item uk-padding-remove" data-numitem="4">
+								<a class="date__link uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Jul, 2018</a>
+							</td>
+							<td class="dates__item uk-padding-remove" data-numitem="5">
+								<a class="date__link uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Ago, 2018</a>
+							</td>
+							<td class="dates__item uk-padding-remove" data-numitem="6">
+								<a class="date__link uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Sep, 2018</a>
+							</td>
+							<td class="dates__item  uk-padding-remove" data-numitem="7">
+								<a class="date__link  uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Oct, 2018</a>
+							</td>
+							<td class="dates__item  uk-padding-remove" data-numitem="8">
+								<a class="date__link  uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Nov, 2018</a>
+							</td>
+							<td class="dates__item  uk-padding-remove" data-numitem="9">
+								<a class="date__link  uk-margin-small-left uk-margin-small-right uk-text-center" href="#">Dec, 2018</a>
+							</td>
+							<td class="dates__item dates__item--end uk-padding-remove"></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<button id="calendar__button-next" class="uk-button uk-button-secondary uk-button-small uk-border-rounded calendar__button">
+			<span uk-icon="icon: chevron-right; ratio: 1.5"></span>
+		</button>
+	</section>
+	<div class="uk-flex-center" uk-grid>
+		<section class="days uk-width-1-1 uk-width-4-5@s uk-width-2-5@m uk-width-3-6@m uk-text-center">
+			<ul class="timeline">
+				<li class="timeline__item">
+					<span class="uk-badge day">30</span>
+					<div class="card uk-card uk-card-small uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+						<div class="card__image uk-card-media-left uk-cover-container">
+							<img src="/images/image_article.jpg" alt="" uk-cover>
+							<canvas width="200" height="150"></canvas>
+						</div>
+						<div>
+							<div class="uk-card-body">
+								<h3 class="uk-card-title"><a href="#">Article Title</a></h3>
+								<span class="uk-text-left uk-text-muted uk-text-small">Septiembre 22, 2018</span>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li class="timeline__item">
+					<span class="uk-badge day">3</span>
+					<div class="card uk-card uk-card-small uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+						<div class="card__image uk-flex-last@s uk-card-media-right uk-cover-container">
+							<img src="/images/image_article.jpg" alt="" uk-cover>
+							<canvas width="200" height="150"></canvas>
+						</div>
+						<div>
+							<div class="uk-card-body">
+								<h3 class="uk-card-title"><a href="#">Article Title</a></h3>
+								<span class="uk-text-left uk-text-muted uk-text-small">Septiembre 22, 2018</span>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li class="timeline__item">
+					<span class="uk-badge day">2</span>
+					<div class="card uk-card uk-card-small uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+						<div class="card__image uk-card-media-left uk-cover-container">
+							<img src="/images/image_article.jpg" alt="" uk-cover>
+							<canvas width="200" height="150"></canvas>
+						</div>
+						<div>
+							<div class="uk-card-body">
+								<h3 class="uk-card-title"><a href="#">Article Title</a></h3>
+								<span class="uk-text-left uk-text-muted uk-text-small">Septiembre 22, 2018</span>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li class="timeline__item">
+					<span class="uk-badge day">1</span>
+					<div class="card uk-card uk-card-small uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+						<div class="card__image uk-flex-last@s uk-card-media-right uk-cover-container">
+							<img src="/images/image_article.jpg" alt="" uk-cover>
+							<canvas width="200" height="150"></canvas>
+						</div>
+						<div>
+							<div class="uk-card-body">
+								<h3 class="uk-card-title"><a href="#">Article Title</a></h3>
+								<span class="uk-text-left uk-text-muted uk-text-small">Septiembre 22, 2018</span>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+							</div>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</section>
+	</div>
+@endsection
