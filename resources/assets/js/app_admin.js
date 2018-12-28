a449ab69750f169eb5d580d101375451a89aca20
Pablo De Jesus Moreno <pablo.24726@gmail.com>
2018-12-28 11:10:39 -0600

    Se integra AdminLTE para el administrador

--- resources/assets/js/app_admin.js
+++ resources/assets/js/app_admin.js
@@ -4,5 +4,7 @@ try {
 	require('./../../../node_modules/admin-lte/bower_components/bootstrap/dist/js/bootstrap');
 
 	require('./../../../node_modules/admin-lte/plugins/iCheck/icheck');
+
+	require('./../../../node_modules/admin-lte/dist/js/adminlte');
     
 } catch (e) {}
---
+++ resources/views/template/administrator/dashboard.blade.php
@@ -0,0 +1,75 @@
+@extends('layout.administrator.layout')
+
+@section('account')
+
+	<li class="dropdown user user-menu">
+		<!-- Menu Toggle Button -->
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+		<!-- The user image in the navbar-->
+		<img src="/images/site/administrator/default-avatar-160.png" class="user-image" alt="User Image">
+		<!-- hidden-xs hides the username on small devices so only the image appears. -->
+		<span class="hidden-xs">Alexander Pierce</span>
+		</a>
+		<ul class="dropdown-menu">
+		<!-- The user image in the menu -->
+		<li class="user-header">
+			<img src="/images/site/administrator/default-avatar-160.png" class="img-circle" alt="User Image">
+
+			<p>
+			Alexander Pierce - Web Developer
+			<small>Miembro desde Nov. 2012</small>
+			</p>
+		</li>
+		{{-- <!-- Menu Body -->
+		<li class="user-body">
+			<div class="row">
+			<div class="col-xs-4 text-center">
+				<a href="#">Followers</a>
+			</div>
+			<div class="col-xs-4 text-center">
+				<a href="#">Sales</a>	
+			</div>
+			<div class="col-xs-4 text-center">
+				<a href="#">Friends</a>
+			</div>
+			</div>
+			<!-- /.row -->
+		</li> --}}
+		<!-- Menu Footer-->
+		<li class="user-footer">
+			<div class="pull-left">
+				<a href="#" class="btn btn-default btn-flat">Profile</a>
+			</div>
+			<div class="pull-right">
+
+				<a href="{{ route('logout') }}"
+					class="btn btn-default btn-flat"
+					onclick="event.preventDefault();
+								document.getElementById('logout-form').submit();">
+					Cerrar Sesión
+				</a>
+
+				<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+					{{ csrf_field() }}
+				</form>
+
+			</div>
+		</li>
+		</ul>
+	</li>
+
+@endsection
+
+
+@section('content')
+
+	<h1>
+		Page Header
+		<small>Optional description</small>
+	</h1>
+	<ol class="breadcrumb">
+		<li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+		<li class="active">Here</li>
+	</ol>
+	
+@endsection
